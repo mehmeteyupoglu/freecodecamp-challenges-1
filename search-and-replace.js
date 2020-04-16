18ca174a4e4f69918ce4a895da4c01b24a0f70ca
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-04-16 16:24:35 +0300

    Add more test materials

--- search-and-replace.js
+++ search-and-replace.js
@@ -17,10 +17,11 @@ console.log(/[A-Z]/.test( before[0]));
 
     if(/[A-Z]/.test( before[0])){
         return str.replace(before, capitalized)
-    } return str.replace(before, after)
-    
+    } return str.replace(before, after) 
   }
-
   
-  console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"))
-  console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting")); 
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"))
+console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"));
+console.log(myReplace("This has a spellngi error", "spellngi", "spelling")) // "This has a spelling error".
+myReplace("His name is Tom", "Tom", "john") //"His name is John".
+myReplace("Let us get back to more Coding", "Coding", "algorithms") // "Let us get back to more Algorithms".
